7893e2e2e355f5a03898eb7bee12f48591f4a252
Kharaberiush <pi_sa_ha@mail.ru>
2026-01-07 15:56:09 +0300

    Initial website release

---
+++ script.js
@@ -0,0 +1,34 @@
+/* --- SHOW MENU --- */
+const navMenu = document.getElementById('nav');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+/* Validate if constants exist */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/* --- REMOVE MENU MOBILE --- */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* --- CHANGE BACKGROUND HEADER --- */
+function scrollHeader() {
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
